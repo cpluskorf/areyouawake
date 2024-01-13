531379825bfff5b0072a19e739a154ca3b1c6e30
Robotnik <robotnik.games@gmail.com>
2024-01-13 16:33:26 +0100

    BIIIG staturday Commit, mamma mia

--- gui/main.py
+++ gui/main.py
@@ -3,38 +3,55 @@ from tkinter import ttk
 
 class GUI:
 
-    def __init__(self) -> None:
+    def __init__(
+            self
+            ) -> None:
         pass
-    
 
     def run(self):
-        root = Tk()
-        root.title("Are you awake?")
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+        # setup root window
+        self.root = Tk()
 
+        # configure parameter and variables
+        self.root.title("Window Title")
+        # self.root.maxsize(960, 540)
         self.answer = StringVar()
-        self.answer_entry = ttk.Entry(mainframe, width=7, textvariable=self.answer)
-        self.answer_entry.grid(column=2, row=1, sticky=(W, E))
 
-        ttk.Button(mainframe, text="Next", command=self._check_answer).grid(column=3, row=3, sticky=W)
+        # create frames
+        self.mainframe = ttk.Frame(self.root).pack()
+        self.top_frame = ttk.Frame(self.mainframe).pack(expand=True, fill=BOTH)
+        self.mid_frame = ttk.Frame(self.mainframe).pack(expand=True, fill=BOTH)
+        self.bottom_frame = ttk.Frame(self.mainframe).pack(expand=True, fill=BOTH)
 
-        for child in mainframe.winfo_children(): 
-            child.grid_configure(padx=5, pady=5)
+        # create Widgets
+        self.question_label = ttk.Label(self.top_frame, text="Here question")
+        self.answer_entry = ttk.Entry(self.mid_frame, textvariable=self.answer)
+        self.next_btn = ttk.Button(self.bottom_frame, text="Next", command=self._check_answer)
+        self.start_btn = ttk.Button(self.bottom_frame, text="Start Timer")
+
+        # configure placement of widgets
+        self.question_label.pack()
+        self.answer_entry.pack()
+        self.next_btn.pack(side=LEFT)
+        self.start_btn.pack(side=RIGHT)
+
+        # for child in mainframe.winfo_children(): 
+        #     child.grid_configure(padx=5, pady=5)
         
+        # configure extras
         self.answer_entry.focus()
-        root.bind("<Return>", self._check_answer)
+        self.root.bind("<Return>", self._check_answer)
 
-        root.mainloop()
+        # starts mainloop
+        self.root.mainloop()
 
     def _check_answer(self):
         # dummy function -> implemented by CPLUSKORF
-        print(self.user_input.get())
+        print(self.answer.get())
 
 
 if __name__ == "__main__":
     test = GUI()
     test.run()
 
+# start btn, next btn . text feld (stattic field at the end)
